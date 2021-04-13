5dbc140d1053d498e347768663b1ba052463049a
cpuCode <923992029@qq.com>
2021-04-13 12:13:29 +0800

    初始化

---
+++ register-parent/src/test/java/com/cpucode/registerparent/RegisterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpucode.registerparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
