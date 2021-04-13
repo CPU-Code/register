998bcab2327505071f693988c5347bbdc05fbfc5
cpuCode <923992029@qq.com>
2021-04-13 17:12:48 +0800

    1、 搭建医院模块service-hosp

---
+++ register-parent/service/service-hosp/src/main/java/com/cpucode/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,18 @@
+package com.cpucode.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : cpucode
+ * @date : 2021/4/13
+ * @time : 13:20
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
