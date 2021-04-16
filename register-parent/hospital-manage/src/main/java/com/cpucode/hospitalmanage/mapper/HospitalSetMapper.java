0b8e11c79b6a548a0a02183ebcbcd6810f7fa104
cpuCode <923992029@qq.com>
2021-04-16 23:23:56 +0800

    上传医院接口

---
+++ register-parent/hospital-manage/src/main/java/com/cpucode/hospitalmanage/mapper/HospitalSetMapper.java
@@ -0,0 +1,13 @@
+package com.cpucode.hospitalmanage.mapper;
+
+import com.cpucode.hospitalmanage.model.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
---
+++ register-parent/hospital-manage/src/main/java/com/cpucode/hospitalmanage/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.cpucode.hospitalmanage.mapper;
+
+import com.cpucode.hospitalmanage.model.Schedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
