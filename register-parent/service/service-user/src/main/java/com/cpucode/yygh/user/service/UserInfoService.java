5ebe660c7eeb65b72d92bd7055c528fe3b8928c5
cpuCode <923992029@qq.com>
2021-04-26 09:49:21 +0800

    会员登录基本完善

--- register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/service/UserInfoService.java
+++ register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/service/UserInfoService.java
@@ -2,6 +2,9 @@ package com.cpucode.yygh.user.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.cpucode.yygh.model.user.UserInfo;
+import com.cpucode.yygh.vo.user.LoginVo;
+
+import java.util.Map;
 
 /**
  * @author : cpucode
@@ -11,5 +14,11 @@ import com.cpucode.yygh.model.user.UserInfo;
  * @csdn : https://blog.csdn.net/qq_44226094
  */
 public interface UserInfoService extends IService<UserInfo> {
+    /**
+     * 会员登录
+     * @param loginVo
+     * @return
+     */
+    Map<String, Object> login(LoginVo loginVo);
 
 }
--- register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/service/impl/UserInfoServiceImpl.java
+++ register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/service/impl/UserInfoServiceImpl.java
@@ -1,10 +1,18 @@
 package com.cpucode.yygh.user.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cpucode.yygh.common.exception.YyghException;
+import com.cpucode.yygh.common.result.ResultCodeEnum;
 import com.cpucode.yygh.model.user.UserInfo;
 import com.cpucode.yygh.user.mapper.UserInfoMapper;
 import com.cpucode.yygh.user.service.UserInfoService;
+import com.cpucode.yygh.vo.user.LoginVo;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author : cpucode
@@ -15,5 +23,64 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements UserInfoService {
+
+    /**
+     * 会员登录
+     */
+    @Override
+    public Map<String, Object> login(LoginVo loginVo) {
+        String phone = loginVo.getPhone();
+        String code = loginVo.getCode();
+
+        //判断手机号和验证码是否为空
+        if(StringUtils.isEmpty(phone) ||
+                StringUtils.isEmpty(code)) {
+            throw new YyghException(ResultCodeEnum.PARAM_ERROR);
+        }
+
+        //TODO 校验校验验证码
+
+        //手机号已被使用
+        QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("phone", phone);
+
+        //获取会员
+        UserInfo userInfo = baseMapper.selectOne(queryWrapper);
+
+        //第一次使用这个手机号登录
+        if(null == userInfo) {
+            userInfo = new UserInfo();
+
+            userInfo.setName("");
+            userInfo.setPhone(phone);
+            userInfo.setStatus(1);
+
+            this.save(userInfo);
+        }
+
+        //校验是否被禁用
+        if(userInfo.getStatus() == 0) {
+            throw new YyghException(ResultCodeEnum.LOGIN_DISABLED_ERROR);
+        }
+
+        //TODO 记录登录
+
+        //返回页面显示名称
+        Map<String, Object> map = new HashMap<>();
+        String name = userInfo.getName();
+
+        if(StringUtils.isEmpty(name)) {
+            name = userInfo.getNickName();
+        }
+        if(StringUtils.isEmpty(name)) {
+            name = userInfo.getPhone();
+        }
+
+        map.put("name", name);
+        map.put("token", "");
+
+        return map;
+    }
+
 
 }
