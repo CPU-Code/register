497b94977e5cec4c372ef45137f6e95a3dc1fc30
cpuCode <923992029@qq.com>
2021-04-26 22:16:31 +0800

    返回微信登录参数

---
+++ register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/api/WeixinApiController.java
@@ -0,0 +1,63 @@
+package com.cpucode.yygh.user.api;
+
+import com.cpucode.yygh.common.result.Result;
+import com.cpucode.yygh.user.service.UserInfoService;
+import com.cpucode.yygh.user.utils.ConstantWxPropertiesUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : cpucode
+ * @date : 2021/4/26
+ * @time : 22:11
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+@Controller
+@RequestMapping("/api/ucenter/wx")
+public class WeixinApiController {
+    @Autowired
+    private UserInfoService userInfoService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    /**
+     *  生成微信扫描二维码
+     * @return 返回生成二维码需要参数
+     */
+    @GetMapping("getLoginParam")
+    @ResponseBody
+    public Result genQrConnect(){
+        try {
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("appid", ConstantWxPropertiesUtils.WX_OPEN_APP_ID);
+            map.put("scope", "snsapi_login");
+
+            String wxOpenRedirectUrl = ConstantWxPropertiesUtils.WX_OPEN_REDIRECT_URL;
+            wxOpenRedirectUrl = URLEncoder.encode(wxOpenRedirectUrl, "utf-8");
+            map.put("redirect_uri",wxOpenRedirectUrl);
+
+            map.put("state", System.currentTimeMillis() + "");
+
+            return Result.ok(map);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+}
