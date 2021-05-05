f7227af0d805d60df70abaaa465b00c94b93e9c6
cpuCode <923992029@qq.com>
2021-05-05 16:53:16 +0800

    就诊人管理接口

--- register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/api/PatientApiController.java
+++ register-parent/service/service-user/src/main/java/com/cpucode/yygh/user/api/PatientApiController.java
@@ -5,9 +5,7 @@ import com.cpucode.yygh.common.utils.AuthContextHolder;
 import com.cpucode.yygh.model.user.Patient;
 import com.cpucode.yygh.user.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -39,4 +37,57 @@ public class PatientApiController {
 
         return Result.ok(list);
     }
+
+    /**
+     * 添加就诊人
+     * @param patient
+     * @param request
+     * @return
+     */
+    @PostMapping("auth/save")
+    public Result savePatient(@RequestBody Patient patient, HttpServletRequest request){
+        //获取当前登录用户id
+        Long userId = AuthContextHolder.getUserId(request);
+        patient.setUserId(userId);
+        patientService.save(patient);
+
+        return Result.ok();
+    }
+
+    /**
+     * 根据id获取就诊人信息
+     * @param id
+     * @return
+     */
+    @GetMapping("auth/get/{id}")
+    public Result getPatient(@PathVariable Long id) {
+        Patient patient = patientService.getPatientId(id);
+
+        return Result.ok(patient);
+    }
+
+    /**
+     * 修改就诊人
+     * @param patient
+     * @return
+     */
+    @PostMapping("auth/update")
+    public Result updatePatient(@RequestBody Patient patient) {
+        patientService.updateById(patient);
+
+        return Result.ok();
+    }
+
+    /**
+     * 删除就诊人
+     * @param id
+     * @return
+     */
+    @DeleteMapping("auth/remove/{id}")
+    public Result removePatient(@PathVariable Long id) {
+        patientService.removeById(id);
+
+        return Result.ok();
+    }
+
 }
